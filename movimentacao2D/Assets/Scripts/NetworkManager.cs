b405b8e0e9b4c1292c8b89d212d26a645a3df802
Henrikilis <73853874+Henrikilis@users.noreply.github.com>
2020-11-21 14:44:41 -0300

    atualizacaodesistema

---
+++ movimentacao2D/Assets/Scripts/NetworkManager.cs
@@ -0,0 +1,41 @@
+﻿using Photon.Pun;
+using Photon.Realtime;
+using UnityEngine;
+
+public class NetworkManager : MonoBehaviourPunCallbacks
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Connect();
+    }
+
+    public void Connect()
+    {
+        PhotonNetwork.ConnectUsingSettings();
+
+    }
+
+    public void Play()
+    {
+        PhotonNetwork.JoinRandomRoom();
+
+    }
+
+    public override void OnJoinRandomFailed(short returnCode, string message)
+    {
+        Debug.Log("tryed to join a room but falied");
+        PhotonNetwork.CreateRoom(null, new RoomOptions { MaxPlayers = 4 });
+    }
+
+    public override void OnJoinedRoom()
+    {
+        Debug.Log("joined room sucessefully");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
